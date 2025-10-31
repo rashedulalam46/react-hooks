b2ad39945ec4768abd88677e966e61964694e787
Rashedul Alam <rashed464@outlook.com>
2025-10-31 23:16:23 +0600

    code updated

--- src/Pages/UseState.jsx
+++ src/Pages/UseState.jsx
@@ -13,7 +13,7 @@ function UseState() {
             <h2>My Name is {Name}</h2>
             <form>
                 <div className="mb-3">
-                    <label htmlFor="typeName" className="form-label">Type Name</label>
+                    <label htmlFor="typeName" className="form-label">Type Your Name</label>
                     <input type="text" className="form-control" id="typeName" value={Name} onChange={(e) => setName(e.target.value)} />
                 </div>
             </form>
@@ -24,7 +24,15 @@ function UseState() {
                 <form>
                     <div className="mb-3">
                         <label htmlFor="name" className="form-label">Name</label>
-                        <input type="text" className="form-control" id="name" value={Name} onChange={(e) => setName(e.target.value)} />
+                        <input type="text" className="form-control" id="name" />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="email" className="form-label">Email</label>
+                        <input type="email" className="form-control" id="email" />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="password" className="form-label">Password</label>
+                        <input type="password" className="form-control" id="password" />
                     </div>
                     <button type="submit" className="btn btn-primary">Submit</button>
                 </form>
