c072ecdf0849e92c3c460bcf6a6067517156a124
Rashedul Alam <rashed464@outlook.com>
2025-10-31 23:14:00 +0600

    code updated

--- src/Pages/UseState.jsx
+++ src/Pages/UseState.jsx
@@ -11,7 +11,24 @@ function UseState() {
         <div>
             <h1>Use State Hook Practice</h1>
             <h2>My Name is {Name}</h2>
+            <form>
+                <div className="mb-3">
+                    <label htmlFor="typeName" className="form-label">Type Name</label>
+                    <input type="text" className="form-control" id="typeName" value={Name} onChange={(e) => setName(e.target.value)} />
+                </div>
+            </form>
+
             <button className='btn btn-primary' onClick={changeName}>Change Name</button>
+            <div className='container'>
+                <h3>Registration form</h3>
+                <form>
+                    <div className="mb-3">
+                        <label htmlFor="name" className="form-label">Name</label>
+                        <input type="text" className="form-control" id="name" value={Name} onChange={(e) => setName(e.target.value)} />
+                    </div>
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </form>
+            </div>
         </div>
     );
 }
